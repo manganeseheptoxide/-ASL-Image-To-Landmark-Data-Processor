fe3a3c6da93f27e101ddd62829da8ded977101fe
Rafael Nico T. Maniquiz <rtmaniquiz@protonmail.com>
2025-02-18 23:06:22 +0800

    processed fsl image data

--- process_data.py
+++ process_data.py
@@ -9,6 +9,7 @@ from data_collection import *
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
 from image_feed import ImageFeed
+import os
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -17,12 +18,12 @@ mp_pose = mp.solutions.pose
 
 def detect():
     
-    # global _landmarks_list, _landmark_connections, _status, _sample
-    # image_folder = f"raw_data\\{_sample}-samples" #The directory where the images are stored
-    # cap = ImageFeed(image_folder = image_folder, loop = False)
+    global _landmarks_list, _landmark_connections, _status, _sample
+    image_folder = f"raw_data\\fsl\\{_sample}" #The directory where the images are stored
+    cap = ImageFeed(image_folder = image_folder, loop = False)
 
-    global _landmarks_list, _landmark_connections, _status, _hand_priority
-    cap = cv2.VideoCapture(0)
+    # global _landmarks_list, _landmark_connections, _status, _hand_priority
+    # cap = cv2.VideoCapture(0)
     with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
         while True:
     
@@ -59,23 +60,27 @@ def detect():
     cap.release()
     cv2.destroyAllWindows()
 
-_landmarks_list, _landmark_connections, _centered_landmark_df = None, None, create_xyz_landmark_df()
-_status, _data_cache = True, queue.Queue()
-_hand_priority = 'left'
-detect()
+# _landmarks_list, _landmark_connections, _centered_landmark_df = None, None, create_xyz_landmark_df()
+# _status, _data_cache = True, queue.Queue()
+# _hand_priority = 'left'
+# detect()
 
-# for letter in range(ord('A'), ord('Z') + 1):
-#   _landmarks_list, _landmark_connections = None, None
-#   _status, _data_cache = True, queue.Queue()
-#   _sample = chr(letter)
-#   detect()
+for letter in range(ord('A'), ord('Z') + 1):
+  _landmarks_list, _landmark_connections = None, None
+  _status, _data_cache = True, queue.Queue()
+  _hand_priority = 'left'
+  _sample = chr(letter)
+  detect()
 
-#   print('Please Wait')
-#   time.sleep(0.5)
+  print('Please Wait')
+  time.sleep(0.5)
 
-#   df = df_entry_from_queue_NLL(df = create_xyz_landmark_df(), data_cache = _data_cache, centered=True, normalize=True)
-#   file_name = f"processed_data_mp01021\\{_sample}_data.csv"
-#   create_csv(df = df, file_name=file_name)
+  df = df_entry_from_queue_NLL(df = create_xyz_landmark_df(), data_cache = _data_cache, centered=True, normalize=True)
+  file_name = f"processed_data\\fsl_mp01021\\{_sample}_data.csv"
+  output_dir = f"processed_data\\fsl_mp01021"
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+  create_csv(df = df, file_name=file_name)
 
 
 
